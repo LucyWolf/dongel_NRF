963b95f009e291783053c0e22928c56174dedb87
Lucy_Wolf <wolfi-luca@hotmail.de>
2026-07-10 18:22:14 +0200

    chore: bump version to 1.2.4

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace Adafruit_LittleFS_Namespace;
 
-#define VERSION            "1.2.3"
+#define VERSION            "1.2.4"
 #ifndef BOARD_LED_PIN
 #define BOARD_LED_PIN      15
 #endif
